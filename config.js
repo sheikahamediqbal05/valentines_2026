f12c6f97d208ca0a6a603dda0ad31ad0ef066340
sheikahamediqbal05 <sheik.ahamed.iqbalw@gmail.com>
2026-02-12 05:04:24 +0800

    Update config.js

--- config.js
+++ config.js
@@ -50,7 +50,7 @@ const CONFIG = {
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now open ur gifttttss on Feb 14th plssss 💅🏻 A big warm cozy hug and MWAHH BABIII!",
+        message: "Now open ur gifttttss plssss 💅🏻 A big warm cozy hug and MWAHH BABIII!",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
